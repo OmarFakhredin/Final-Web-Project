37575f95487c0ca923ff533cde866431e7f5997c
Omar_Fakhredin <omarfe123@gmail.com>
2024-05-08 13:00:00 +0300

    fixing some errors and not allowing add admin if not logged in

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ https://templatemo.com/tm-589-lugx-gaming
         <div class="col-12">
           <nav class="main-nav">
             <!-- ***** Logo Start ***** -->
-            <a href="index.html" class="logo">
+            <a href="index.php" class="logo">
               <img src="FE/assets/images/logo.png" alt="" style="width: 158px;">
             </a>
             <!-- ***** Logo End ***** -->
