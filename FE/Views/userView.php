9427995cd7b714b22f56ea409ba2a7f982e600d0
Omar_Fakhredin <omarfe123@gmail.com>
2024-05-08 12:25:51 +0300

    adding the userController on the frontend side

--- FE/Views/userView.php
+++ FE/Views/userView.php
@@ -31,24 +31,7 @@ function LoginForm(){
             </div>
         </div>
     </div>
-
-    <script>
-        function LoginFrm(){
-            let un=document.getElementById("tfun").value;
-            console.log(un);
-            let pass=document.getElementById("tfpass").value;
-            console.log(pass);
-            if ((un=="")||(pass=="")){
-                alert("Please fill in the username and password");
-            } else {
-                document.getElementById("login-frm").submit();
-            }
-        }
-        function ResetFrm(){
-            document.getElementById("tfun").value="";
-            document.getElementById("tfpass").value="";
-        }
-    </script>
+    <?php include_once("../controllers/userController.php"); ?>
         
     <?php
 }
